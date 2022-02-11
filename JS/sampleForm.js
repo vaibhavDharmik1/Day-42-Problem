33d0a7a0b2a82091ce46c71ca5f55cb4c98d346e
Vaibhav Dharmik <95203957+vaibhavDharmik1@users.noreply.github.com>
2022-02-11 09:26:53 +0530

    Day 42 of Sample Form

---
+++ JS/sampleForm.js
@@ -0,0 +1,28 @@
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() {
+    output.textContent = salary.value;
+});
+
+const text = document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function() {
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if(nameRegex.test(text.value))
+        textError.textContent = "";
+    else textError.textContent = "Name is Incorrect";
+});
+
+/**
+ * To validate the email field using the REGEX expression
+ */
+
+ const email = document.querySelector('#email');
+ const emailError = document.querySelector('.email-error');
+ text.addEventListener('input', function() {
+     let emailRegex = RegExp("^[A-Za-z0-9]*[@][a-z]{5}[.][a-z]{3}$");
+     if(emailRegex.test(email.value))
+         emailError.emailContent = "";
+     else emailError.emailContent = "Email is Incorrect";
+ });
